189de22e3707d2d42525a06ed12904bc9030f859
sendyTheDev <phathisanisendy@gmail.com>
2023-09-16 19:49:55 -0700

    object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+the class City. If the State object is deleted
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ class class """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', cascade="all, delete", backref='state')
